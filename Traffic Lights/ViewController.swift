cdd748bcff94884474d419b6ed0870dd42fa89ce
JlblCblU4EPT <sashabarsasha228@yandex.ru>
2022-03-27 17:18:12 +0800

    Fix constraints

--- Traffic Lights/ViewController.swift
+++ Traffic Lights/ViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redView: UIView!
+    @IBOutlet weak var yellowView: UIView!
+    @IBOutlet weak var greenView: UIView!
+    @IBOutlet weak var startButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redView.layer.cornerRadius = 65
+        yellowView.layer.cornerRadius = 65
+        greenView.layer.cornerRadius = 65
+        startButton.layer.cornerRadius = 15
     }
 
-
+    @IBAction func pushStartButton() {
+        redView.alpha = 1
+    }
+    
 }
 
