ceda8bf82aac3e54622a10b7a05f615a5c52c48e
JlblCblU4EPT <sashabarsasha228@yandex.ru>
2022-03-27 22:05:25 +0800

    Сделать функционал кнопки

--- Traffic Lights/ViewController.swift
+++ Traffic Lights/ViewController.swift
@@ -12,7 +12,10 @@ class ViewController: UIViewController {
     @IBOutlet weak var redView: UIView!
     @IBOutlet weak var yellowView: UIView!
     @IBOutlet weak var greenView: UIView!
+    
     @IBOutlet weak var startButton: UIButton!
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,7 +26,25 @@ class ViewController: UIViewController {
     }
 
     @IBAction func pushStartButton() {
-        redView.alpha = 1
+
+        if startButton.currentTitle == "START" {
+            startButton.setTitle("NEXT", for: .normal)
+        }
+        
+        if isLightOn(redView) {
+            redView.alpha = 0.3
+            yellowView.alpha = 1
+        } else if isLightOn(yellowView) {
+            yellowView.alpha = 0.3
+            greenView.alpha = 1
+        } else {
+            greenView.alpha = 0.3
+            redView.alpha = 1
+        }
+    }
+    
+    func isLightOn(_ color: UIView!) -> Bool {
+        color.alpha == 1 ? true : false
     }
     
 }
